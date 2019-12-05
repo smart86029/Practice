f86a5f027cdcd40aab926f7b442a7dc9ea58e425
smart86029 <smart86029@gmail.com>
2019-12-05 16:51:20 +0800

    Set Mismatch

---
+++ test/Practice.LeetCode.Tests/LeetCode0645Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0645Tests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 2, 4 }, new int[] { 2, 3 })]
+        [DataRow(new int[] { 1, 1 }, new int[] { 1, 2 })]
+        public void FindErrorNumsTest(int[] nums, int[] expected)
+        {
+            var leetCode = new LeetCode0645();
+            var output = leetCode.FindErrorNums(nums);
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
