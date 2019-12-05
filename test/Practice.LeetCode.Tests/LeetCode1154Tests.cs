9220d70941f91936372e247cca750845264e555d
smart86029 <smart86029@gmail.com>
2019-12-05 14:49:57 +0800

    Day of the Year

---
+++ test/Practice.LeetCode.Tests/LeetCode1154Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode1154Tests
+    {
+        [DataTestMethod]
+        [DataRow("2019-01-09", 9)]
+        [DataRow("2019-02-10", 41)]
+        [DataRow("2003-03-01", 60)]
+        [DataRow("2004-03-01", 61)]
+        public void DayOfYearTest(string date, int expected)
+        {
+            var leetCode = new LeetCode1154();
+            var output = leetCode.DayOfYear(date);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
