28cf234c6db2b56ff8b0fd8775e25a62a39909c0
smart86029 <smart86029@gmail.com>
2019-12-06 14:34:30 +0800

    LeetCode1269Tests

---
+++ src/Practice.LeetCode/LeetCode1269.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LeetCode
+{
+    public class LeetCode1269
+    {
+        public int NumWays(int steps, int arrLen)
+        {
+            var result = 0;
+
+            if (steps % 2 == 0)
+            {
+                for (var i = 0; i < arrLen; i += 2)
+                {
+                    var move = (arrLen - i) / 2;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode1269Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode1269Tests
+    {
+        [DataTestMethod]
+        [DataRow(3, 2, 4)]
+        public void NumWays(int steps, int arrLen, int expected)
+        {
+            var leetCode = new LeetCode1269();
+            var output = leetCode.NumWays(steps, arrLen);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
