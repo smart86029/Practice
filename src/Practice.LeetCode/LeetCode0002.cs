d5064cd48a722f1c401ecb4351fd906bff642ce7
smart86029 <smart86029@gmail.com>
2019-12-06 17:14:48 +0800

    Add Two Numbers

---
+++ src/Practice.LeetCode/LeetCode0002.cs
@@ -0,0 +1,50 @@
+﻿namespace Practice.LeetCode
+{
+    public class LeetCode0002
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var result = default(ListNode);
+            var last = default(ListNode);
+            var temp1 = l1;
+            var temp2 = l2;
+            var carry = 0;
+            while (temp1 != default || temp2 != default || carry > 0)
+            {
+                var sum = carry;
+                if (temp1 != default)
+                {
+                    sum += temp1.val;
+                    temp1 = temp1.next;
+                }
+                if (temp2 != default)
+                {
+                    sum += temp2.val;
+                    temp2 = temp2.next;
+                }
+
+                var current = new ListNode(sum % 10);
+                carry = sum / 10;
+                if (last != default)
+                    last.next = current;
+
+                last = current;
+                if (result == default)
+                    result = current;
+            }
+
+            return result;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode0002Tests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Practice.LeetCode.LeetCode0002;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0002Tests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 }, new int[] { 7, 0, 8 })]
+        [DataRow(new int[] { 5 }, new int[] { 5 }, new int[] { 0, 1 })]
+        public void AddTwoNumbersTests(int[] nums1, int[] nums2, int[] expected)
+        {
+            var l1 = ToListNode(nums1);
+            var l2 = ToListNode(nums2);
+            var leetCode = new LeetCode0002();
+            var output = leetCode.AddTwoNumbers(l1, l2);
+            var actual = FromListNode(output);
+            CollectionAssert.AreEqual(expected, actual);
+
+            ListNode ToListNode(int[] nums)
+            {
+                var result = default(ListNode);
+                var temp = default(ListNode);
+                foreach (var i in nums)
+                {
+                    var node = new ListNode(i);
+                    if (temp != default)
+                        temp.next = node;
+                    else
+                        result = node;
+
+                    temp = node;
+                }
+
+                return result;
+            }
+
+            int[] FromListNode(ListNode node)
+            {
+                var nums = new List<int>();
+                var temp = node;
+                while (temp != default)
+                {
+                    nums.Add(temp.val);
+                    temp = temp.next;
+                }
+
+                return nums.ToArray();
+            }
+        }
+    }
+}
