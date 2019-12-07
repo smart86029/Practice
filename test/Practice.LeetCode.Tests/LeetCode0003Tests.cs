4f030552ac8d022520b78cbb2c1b586efb13a0fc
smart86029 <smart86029@gmail.com>
2019-12-07 19:40:26 +0800

    Longest Substring Without Repeating Characters

---
+++ test/Practice.LeetCode.Tests/LeetCode0003Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0003Tests
+    {
+        [DataTestMethod]
+        [DataRow("abcabcbb", 3)]
+        [DataRow("bbbbb", 1)]
+        [DataRow("pwwkew", 3)]
+        [DataRow(" ", 1)]
+        public void LengthOfLongestSubstringTests(string s, int expected)
+        {
+            var leetCode = new LeetCode0003();
+            var output = leetCode.LengthOfLongestSubstring(s);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
