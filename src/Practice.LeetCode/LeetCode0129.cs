acf74729923c2fa4fc98e40047517a9aa7d7b4e0
smart86029 <smart86029@gmail.com>
2019-12-10 15:30:37 +0800

    Sum Root to Leaf Numbers

---
+++ src/Practice.LeetCode/LeetCode0129.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice.LeetCode
+{
+    public class LeetCode0129
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            if (root == default)
+                return 0;
+
+            var result = 0;
+            var unvisited = new Stack<Tuple<TreeNode, int>>();
+            unvisited.Push(Tuple.Create(root, 0));
+            while (unvisited.Any())
+            {
+                var pop = unvisited.Pop();
+                var current = pop.Item1;
+                var sum = pop.Item2 * 10 + current.val;
+                if (current.right != default)
+                    unvisited.Push(Tuple.Create(current.right, sum));
+                if (current.left != default)
+                    unvisited.Push(Tuple.Create(current.left, sum));
+                if (current.right == default && current.left == default)
+                    result += sum;
+            }
+
+            return result;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode0129Tests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Practice.LeetCode.LeetCode0129;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0129Tests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, 25)]
+        [DataRow(new int[] { }, 0)]
+        public void SumNumbersTest(int[] nums, int expected)
+        {
+            var leetCode = new LeetCode0129();
+            var output = leetCode.SumNumbers(ToTreeNode(nums));
+            Assert.AreEqual(expected, output);
+
+            TreeNode ToTreeNode(int[] nums)
+            {
+                if (nums.Length == 0)
+                    return default;
+
+                var count = nums.Length;
+                var nodes = new TreeNode[count];
+
+                for (var i = 0; i < count; i++)
+                {
+                    var leftIndex = i * 2 + 1;
+                    var rightIndex = leftIndex + 1;
+
+                    if (nodes[i] == default)
+                        nodes[i] = new TreeNode(0);
+
+                    nodes[i].val = nums[i];
+                    if (leftIndex < count)
+                    {
+                        nodes[leftIndex] = new TreeNode(0);
+                        nodes[i].left = nodes[leftIndex];
+                    }
+                    if (rightIndex < count)
+                    {
+                        nodes[rightIndex] = new TreeNode(0);
+                        nodes[i].right = nodes[rightIndex];
+                    }
+                }
+
+                return nodes[0];
+            }
+        }
+    }
+}
