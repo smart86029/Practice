3ebab9d8d28f6cc9d2ca8a2429647373b32f8269
smart86029 <smart86029@gmail.com>
2019-12-16 16:20:08 +0800

    Longest Common Prefix

---
+++ src/Practice.LeetCode/LeetCode0014.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice.LeetCode
+{
+    public class LeetCode0014
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return string.Empty;
+
+            var first = strs[0];
+            for (var i = 0; i < first.Length; i++)
+            {
+                foreach (var str in strs)
+                {
+                    if (str.Length <= i || first[i] != str[i])
+                        return first.Substring(0, i);
+                }
+            }
+
+            return first;
+        }
+    }
+}
