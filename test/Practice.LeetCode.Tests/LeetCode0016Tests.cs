8212ff30fdd819f803a149f240e559e59300a187
smart86029 <smart86029@gmail.com>
2019-12-17 16:03:59 +0800

    3Sum Closest

---
+++ test/Practice.LeetCode.Tests/LeetCode0016Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0016Tests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { -1, 2, 1, -4 }, 1, 2)]
+        [DataRow(new int[] { 1, 1, -1, -1, 3 }, -1, -1)]
+        public void ThreeSumClosestTest(int[] nums, int target, int expected)
+        {
+            var leetCode = new LeetCode0016();
+            var output = leetCode.ThreeSumClosest(nums, target);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
