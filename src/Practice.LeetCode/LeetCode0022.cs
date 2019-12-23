229fc85915c63edd007aa94e49e58be415fd0f48
smart86029 <smart86029@gmail.com>
2019-12-23 17:21:26 +0800

    Generate Parentheses

---
+++ src/Practice.LeetCode/LeetCode0022.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Practice.LeetCode
+{
+    public class LeetCode0022
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var result = new List<string>();
+            Backtrack(result, string.Empty, 0, 0, n);
+
+            return result;
+        }
+
+        private void Backtrack(List<string> result, string current, int open, int close, int max)
+        {
+            if (current.Length == max * 2)
+            {
+                result.Add(current);
+                return;
+            }
+
+            if (open < max)
+                Backtrack(result, current + "(", open + 1, close, max);
+            if (close < open)
+                Backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode0022Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0022Tests
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(new[] { "((()))", "(()())", "(())()", "()(())", "()()()" }, 3)]
+        public void GenerateParenthesisTest(string[] expected, int n)
+        {
+            var leetCode = new LeetCode0022();
+            var output = leetCode.GenerateParenthesis(n);
+            CollectionAssert.AreEqual(expected, output.ToArray());
+        }
+    }
+}
