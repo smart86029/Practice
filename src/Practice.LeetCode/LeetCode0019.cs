a60f88b307cdd8e4a01b64adf6f355d2692ee439
smart86029 <smart86029@gmail.com>
2019-12-26 10:10:59 +0800

    Remove Nth Node From End of List

---
+++ src/Practice.LeetCode/LeetCode0019.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Practice.LeetCode
+{
+    public class LeetCode0019
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var nodes = new List<ListNode>();
+            var curent = head;
+            while (curent != default)
+            {
+                nodes.Add(curent);
+                curent = curent.next;
+            }
+
+            var remove = nodes.Count - n;
+            if (remove - 1 >= 0)
+                nodes[remove - 1].next = nodes[remove].next;
+            else
+                return nodes[remove].next;
+
+            return nodes[0];
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode0019Tests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Practice.LeetCode.LeetCode0019;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0019Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 1, 2, 3, 4, 5 }, 2, new[] { 1, 2, 3, 5 })]
+        public void RemoveNthFromEndTest(int[] nums, int n, int[] expected)
+        {
+            var head = ToListNode(nums);
+            var leetCode = new LeetCode0019();
+            var output = leetCode.RemoveNthFromEnd(head, n);
+            CollectionAssert.AreEqual(expected, FromListNode(output));
+
+            ListNode ToListNode(int[] nums)
+            {
+                var result = default(ListNode);
+                var temp = default(ListNode);
+                foreach (var num in nums)
+                {
+                    var node = new ListNode(num);
+                    if (temp != default)
+                        temp.next = node;
+                    else
+                        result = node;
+
+                    temp = node;
+                }
+
+                return result;
+            }
+
+            int[] FromListNode(ListNode node)
+            {
+                var nums = new List<int>();
+                var temp = node;
+                while (temp != default)
+                {
+                    nums.Add(temp.val);
+                    temp = temp.next;
+                }
+
+                return nums.ToArray();
+            }
+        }
+    }
+}
