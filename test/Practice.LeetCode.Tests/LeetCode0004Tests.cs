77e05c79a08ea24a620acca052648446266f56b2
smart86029 <smart86029@gmail.com>
2019-12-26 14:41:06 +0800

    Median of Two Sorted Arrays

---
+++ test/Practice.LeetCode.Tests/LeetCode0004Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0004Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 1, 3 }, new[] { 2 }, 2)]
+        [DataRow(new[] { 1, 2 }, new[] { 3, 4 }, 2.5)]
+        public void FindMedianSortedArraysTest(int[] nums1, int[] nums2, double expected)
+        {
+            var leetCode = new LeetCode0004();
+            var output = leetCode.FindMedianSortedArrays(nums1, nums2);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
