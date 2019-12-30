0dbd556384a22ff5c82af5852f141a9685a1c8cb
smart86029 <smart86029@gmail.com>
2019-12-30 18:04:21 +0800

    Palindrome Number

---
+++ src/Practice.LeetCode/LeetCode0009.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice.LeetCode
+{
+    public class LeetCode0009
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            var temp = x;
+            var reverse = 0;
+
+            while (temp > 0)
+            {
+                reverse = reverse * 10 + temp % 10;
+                temp /= 10;
+            }
+
+            return x == reverse;
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode0009Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0009Tests
+    {
+        [DataTestMethod]
+        [DataRow(121, true)]
+        [DataRow(-121, false)]
+        [DataRow(10, false)]
+        public void ConvertTest(int x, bool expected)
+        {
+            var leetCode = new LeetCode0009();
+            var output = leetCode.IsPalindrome(x);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
