026f1760abb2678765b19642f7094847efa0de53
smart86029 <smart86029@gmail.com>
2019-12-30 10:44:13 +0800

    Reverse Integer

---
+++ test/Practice.LeetCode.Tests/LeetCode0007Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0007Tests
+    {
+        [DataTestMethod]
+        [DataRow(123, 321)]
+        [DataRow(-123, -321)]
+        [DataRow(120, 21)]
+        [DataRow(int.MinValue, 0)]
+        [DataRow(1534236469, 0)]
+        public void ConvertTest(int x, int expected)
+        {
+            var leetCode = new LeetCode0007();
+            var output = leetCode.Reverse(x);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
