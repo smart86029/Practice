8b93bc0e92f9d259d81b8535b924259701e5cee8
smart86029 <smart86029@gmail.com>
2019-12-31 10:30:51 +0800

    Container With Most Water

---
+++ test/Practice.LeetCode.Tests/LeetCode0011Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0011Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        public void MaxAreaTest(int[] height, int expected)
+        {
+            var leetCode = new LeetCode0011();
+            var output = leetCode.MaxArea(height);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
