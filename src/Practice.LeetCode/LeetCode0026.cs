c8c99f2ec017ed20075d5c1a998a293b16722132
smart86029 <smart86029@gmail.com>
2020-01-06 15:15:20 +0800

    Remove Duplicates from Sorted Array

---
+++ src/Practice.LeetCode/LeetCode0026.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice.LeetCode
+{
+    public class LeetCode0026
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == default || nums.Length == 0)
+                return 0;
+
+            var next = 0;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[next])
+                {
+                    next++;
+                    nums[next] = nums[i];
+                }
+            }
+
+            return next + 1;
+        }
+    }
+}
