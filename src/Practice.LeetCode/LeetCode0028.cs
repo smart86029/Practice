a578f088ab6d9441c7fcfac32de58e4904e934b9
smart86029 <smart86029@gmail.com>
2020-01-07 14:56:53 +0800

    Implement strStr()

---
+++ src/Practice.LeetCode/LeetCode0028.cs
@@ -0,0 +1,29 @@
+﻿namespace Practice.LeetCode
+{
+    public class LeetCode0028
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle == string.Empty)
+                return 0;
+
+            for (var i = 0; i < haystack.Length - needle.Length + 1; i++)
+            {
+                var allSame = true;
+                for (var j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        allSame = false;
+                        break;
+                    }
+                }
+
+                if (allSame)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
