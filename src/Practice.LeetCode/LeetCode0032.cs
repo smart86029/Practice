22ccc6ad4c9984e198745191722658fc9dc4f6a5
smart86029 <smart86029@gmail.com>
2020-03-10 10:49:36 +0800

    Longest Valid Parentheses

---
+++ src/Practice.LeetCode/LeetCode0032.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice.LeetCode
+{
+    public class LeetCode0032
+    {
+        public int LongestValidParentheses(string s)
+        {
+            var max = 0;
+            var stack = new Stack<int>();
+            stack.Push(-1);
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    stack.Pop();
+                    if (!stack.Any())
+                        stack.Push(i);
+                    else
+                        max = Math.Max(max, i - stack.Peek());
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode0032Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0032Tests
+    {
+        [DataTestMethod]
+        [DataRow("(()", 2)]
+        [DataRow(")()())", 4)]
+        public void NextPermutationTest(string s, int expected)
+        {
+            var leetCode = new LeetCode0032();
+            var output = leetCode.LongestValidParentheses(s);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
