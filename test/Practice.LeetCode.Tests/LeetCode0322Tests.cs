ecd56a1bf3aa6c99abbf06969ac46077dc872a30
smart86029 <smart86029@gmail.com>
2020-04-16 17:47:52 +0800

    Coin Change

---
+++ test/Practice.LeetCode.Tests/LeetCode0322Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0322Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 1, 2, 5 }, 11, 3)]
+        [DataRow(new[] { 2 }, 3, -1)]
+        public void CoinChangeTest(int[] coins, int amount, int expected)
+        {
+            var leetCode = new LeetCode0322();
+            var output = leetCode.CoinChange(coins, amount);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
