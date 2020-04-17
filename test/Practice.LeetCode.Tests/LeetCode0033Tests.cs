742410341d9f06dde74c90f5139baa93f8bfd7a0
smart86029 <smart86029@gmail.com>
2020-04-17 16:14:59 +0800

    Search in Rotated Sorted Array

---
+++ test/Practice.LeetCode.Tests/LeetCode0033Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0033Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+        [DataRow(new[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+        public void SearchTest(int[] nums, int target, int expected)
+        {
+            var leetCode = new LeetCode0033();
+            var output = leetCode.Search(nums, target);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
