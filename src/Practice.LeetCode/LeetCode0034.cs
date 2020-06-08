57b2e3525e0fafaa3380677da6f92e5ca5cc2d61
smart86029 <smart86029@gmail.com>
2020-06-08 11:38:38 +0800

    Find First and Last Position of Element in Sorted Array

---
+++ src/Practice.LeetCode/LeetCode0034.cs
@@ -0,0 +1,46 @@
+﻿namespace Practice.LeetCode
+{
+    public class LeetCode0034
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var result = new[] { -1, -1 };
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                var middle = left + (right - left) / 2;
+
+                if (nums[middle] == target && (middle == 0 || nums[middle - 1] < target))
+                {
+                    result[0] = middle;
+                    break;
+                }
+                else if (nums[right] >= target && nums[middle] < target)
+                    left = middle + 1;
+                else
+                    right = middle - 1;
+            }
+
+            right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                var middle = left + (right - left) / 2;
+
+                if (nums[middle] == target && (middle == nums.Length - 1 || nums[middle + 1] > target))
+                {
+                    result[1] = middle;
+                    break;
+                }
+                else if (nums[left] <= target && nums[middle] > target)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+
+            return result;
+        }
+    }
+}
