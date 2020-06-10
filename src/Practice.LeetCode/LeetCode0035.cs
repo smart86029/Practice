337a30d3c627f3b36fd398cea1e8e936b941ffae
smart86029 <smart86029@gmail.com>
2020-06-10 16:27:47 +0800

    Search Insert Position

---
+++ src/Practice.LeetCode/LeetCode0035.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice.LeetCode
+{
+    public class LeetCode0035
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                var middle = left + (right - left) / 2;
+
+                if (nums[middle] >= target)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+
+            return left;
+        }
+    }
+}
