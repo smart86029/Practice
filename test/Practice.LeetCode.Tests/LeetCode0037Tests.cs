7b4b3e824b2a1080baa5c1fed836004afc1c3ba9
smart86029 <smart86029@gmail.com>
2020-06-11 17:36:59 +0800

    Sudoku Solver

---
+++ test/Practice.LeetCode.Tests/LeetCode0037Tests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0037Tests
+    {
+        private static IEnumerable<object[]> Data =>
+            new[]
+            {
+                new object[]
+                {
+                    new[]
+                    {
+                        new[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                        new[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                        new[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                        new[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                        new[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                        new[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                        new[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                        new[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                        new[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' },
+                    },
+                },
+            };
+
+        [DataTestMethod]
+        [DynamicData(nameof(Data))]
+        public void SolveSudokuTest(char[][] board)
+        {
+            var leetCode = new LeetCode0037();
+            leetCode.SolveSudoku(board);
+        }
+    }
+}
