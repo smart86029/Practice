cbdd98096b720314f721012f78016084de1436f3
Bill Hsu <smart86029@gmail.com>
2021-03-21 15:08:41 +0800

    N-Queens

---
+++ test/Practice.LeetCode.Tests/LeetCode0051Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0051Tests
+    {
+        [TestMethod]
+        public void SolveNQueensTest()
+        {
+            var leetCode = new LeetCode0051();
+            var expected = new List<IList<string>>
+            {
+                new List<string> { "Q" },
+            } as IList<IList<string>>;
+            var output = leetCode.SolveNQueens(8);
+
+            Console.WriteLine(output[0]);
+        }
+    }
+}
