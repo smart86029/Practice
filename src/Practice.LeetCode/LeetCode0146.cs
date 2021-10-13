1511abec48f38d21d8c30cc2977de3b8021ce8ac
Bill Hsu <smart86029@gmail.com>
2021-10-13 21:18:30 +0800

    LRU Cache

---
+++ src/Practice.LeetCode/LeetCode0146.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+
+namespace Practice.LeetCode
+{
+    public class LRUCache
+    {
+        private int capacity;
+        private Dictionary<int, Node> dictionary = new Dictionary<int, Node>();
+        private DoubleList doubleList = new DoubleList();
+
+        public LRUCache(int capacity)
+        {
+            this.capacity = capacity;
+        }
+
+        public int Get(int key)
+        {
+            if (dictionary.TryGetValue(key, out var node))
+            {
+                doubleList.Remove(node);
+                doubleList.AddFirst(node);
+                return node.Value;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void Put(int key, int value)
+        {
+            if (dictionary.TryGetValue(key, out var node))
+            {
+                doubleList.Remove(node);
+                var newNode = new Node(key, value);
+                dictionary[key] = newNode;
+                doubleList.AddFirst(newNode);
+            }
+            else
+            {
+                if (doubleList.Count >= capacity)
+                {
+                    var last = doubleList.RemoveLast();
+                    dictionary.Remove(last.Key);
+                }
+
+                var newNode = new Node(key, value);
+                dictionary.Add(key, newNode);
+                doubleList.AddFirst(newNode);
+            }
+        }
+
+        private class Node
+        {
+            public Node(int key, int value)
+            {
+                Key = key;
+                Value = value;
+            }
+
+            public int Key { get; private set; }
+
+            public int Value { get; private set; }
+
+            public Node Next { get; set; }
+
+            public Node Previous { get; set; }
+        }
+
+        private class DoubleList
+        {
+            private Node head = new Node(0, 0);
+            private Node tail = new Node(0, 0);
+
+            public DoubleList()
+            {
+                head.Next = tail;
+                tail.Previous = head;
+            }
+
+            public int Count { get; private set; }
+
+            public void AddFirst(Node node)
+            {
+                var first = head.Next;
+                first.Previous = node;
+                node.Next = first;
+                head.Next = node;
+                node.Previous = head;
+                Count++;
+            }
+
+            public Node RemoveLast()
+            {
+                if (Count == 0)
+                {
+                    return null;
+                }
+
+                var last = tail.Previous;
+                Remove(last);
+                return last;
+            }
+
+            public void Remove(Node node)
+            {
+                var previous = node.Previous;
+                var next = node.Next;
+                previous.Next = next;
+                next.Previous = previous;
+                Count--;
+            }
+        }
+    }
+}
---
+++ test/Practice.LeetCode.Tests/LeetCode0146Tests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice.LeetCode.Tests
+{
+    [TestClass]
+    public class LeetCode0146Tests
+    {
+        [TestMethod]
+        public void LRUCacheTest1()
+        {
+            var inputs = new (string Action, int[] Param)[]
+            {
+                ("put", new[] { 1, 1 }),
+                ("put", new[] { 2, 2 }),
+                ("get", new[] { 1 }),
+                ("put", new[] { 3, 3 }),
+                ("get", new[] { 2 }),
+                ("put", new[] { 4, 4 }),
+                ("get", new[] { 1 }),
+                ("get", new[] { 3 }),
+                ("get", new[] { 4 }),
+            };
+            var expected = new int?[] { null, null, 1, null, -1, null, -1, 3, 4 };
+            var actual = Execute(inputs);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LRUCacheTest2()
+        {
+            var inputs = new (string Action, int[] Param)[]
+            {
+                ("put", new[] { 2, 1 }),
+                ("put", new[] { 2, 2 }),
+                ("get", new[] { 2 }),
+                ("put", new[] { 1, 1 }),
+                ("put", new[] { 4, 1 }),
+                ("get", new[] { 2 }),
+            };
+            var expected = new int?[] { null, null, 2, null, null, -1 };
+            var actual = Execute(inputs);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        private int?[] Execute((string Action, int[] Param)[] inputs)
+        {
+            var result = new int?[inputs.Length];
+            var cache = new LRUCache(2);
+
+            for (var i = 0; i < inputs.Length; i++)
+            {
+                var input = inputs[i];
+                if (input.Action == "put")
+                {
+                    cache.Put(input.Param[0], input.Param[1]);
+                    result[i] = null;
+                }
+                else if (input.Action == "get")
+                {
+                    result[i] = cache.Get(input.Param[0]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
